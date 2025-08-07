e44a44e2e648433bab7ba339c7110e9608345c2c
DagmawiKK <dagmawikassa1@gmail.com>
2025-08-07 12:18:17 +0300

    add generate idea end point

--- delivery/routers/blog_routers.go
+++ delivery/routers/blog_routers.go
@@ -8,4 +8,5 @@ import (
 func RegisterBlogRoutes(router *gin.Engine, blogController *controllers.BlogController) {
 	router.POST("/blogs", blogController.CreateBlog)
 	router.GET("/blogs", blogController.GetBlogs)
+	router.POST("/blogs/ideas", blogController.GenerateBlogIdeas)
 }
