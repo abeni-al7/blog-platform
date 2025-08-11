c0d5c9931c436383a1a54c7ab9ac2e08bf74b593
Binyam Abrha Abay <176568730+binia01@users.noreply.github.com>
2025-08-11 14:25:50 +0300

    refactor: restructure blog route registration and enhance middleware integration

--- delivery/routers/blog_routers.go
+++ delivery/routers/blog_routers.go
@@ -1,18 +1,36 @@
 package routers
 
 import (
+	"os"
+
 	"github.com/blog-platform/delivery/controllers"
+	"github.com/blog-platform/infrastructure"
+	"github.com/blog-platform/repositories"
+	"github.com/blog-platform/usecases"
 	"github.com/gin-gonic/gin"
 )
 
-func RegisterBlogRoutes(router *gin.Engine, blogController *controllers.BlogController) {
-  
-	router.POST("/blogs", blogController.CreateBlog)
-	router.GET("/blogs/:id", blogController.GetBlogByID)
-	router.GET("/blogs", blogController.GetBlogs)
-	router.DELETE("/blogs/:id", blogController.DeleteBlog)
-	router.GET("/blogs/paginated", blogController.FetchPaginatedBlogs)
-  router.POST("/blogs/ideas", blogController.GenerateBlogIdeas)
-	router.POST("/blogs/improve", blogController.SuggestBlogImprovements)
-  
+func BlogRoutes(router *gin.RouterGroup) {
+
+	DB := repositories.DB
+	ur := repositories.NewBlogRepository(DB)
+	tr := repositories.NewTokenRepository(DB)
+	js := infrastructure.NewJWTInfrastructure([]byte(os.Getenv("JWT_ACCESS_SECRET")), []byte(os.Getenv("JWT_REFRESH_SECRET")), tr)
+	ao := infrastructure.NewMiddleware(js)
+	ai := infrastructure.NewChatGPTAIService()
+	uu := usecases.NewBlogUsecase(ur, ai)
+	uc := controllers.NewBlogController(uu)
+
+	blogRoutes := router.Group("/blogs")
+	blogRoutes.Use(ao.AuthMiddleware())
+	{
+		blogRoutes.POST("", uc.CreateBlog)
+		blogRoutes.GET("/:id", uc.GetBlogByID)
+		blogRoutes.GET("", uc.GetBlogs)
+		blogRoutes.DELETE("/:id", uc.DeleteBlog)
+		blogRoutes.GET("/paginated", uc.FetchPaginatedBlogs)
+		blogRoutes.POST("/ideas", uc.GenerateBlogIdeas)
+		blogRoutes.POST("/improve", uc.SuggestBlogImprovements)
+	}
+
 }
